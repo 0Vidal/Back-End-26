b6bdf68684284587d6e2512bc05123d97e52c1f6
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-06-24 09:49:18 -0300

    Add student details to aula1.java

--- AulaJava/aula1.java
+++ AulaJava/aula1.java
@@ -5,5 +5,14 @@ public class aula1{
 
         System.out.println("Olá Mundo");
 
+        int idade = 17;
+        double altura = 1.75;
+        boolean matriculado = true;
+        String nome = "João Sila";
+
+       System.out.println("Nome: " + nome);
+       System.out.println("Idade: " + idade + " anos");
+       System.out.println("Altura: " + altura);
+       System.out.println("Aluno Matriculado: " + matriculado);
     }
 }
