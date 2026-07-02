c2fd23b5a6a9019468bcafe0d169fd1e1eef887e
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-07-02 08:46:58 -0300

    Add files via upload

---
+++ AulaJava/02_orientacao_objeto/Aula11_MetodoParametros.java
@@ -0,0 +1,6 @@
+public class Aula11_MetodoParametros {
+    public static void main(String[] args) {
+        CalculadoraSimples cal = new CalculadoraSimples();
+        cal.SomarDoisNumeros(12, 30);
+    }
+}
