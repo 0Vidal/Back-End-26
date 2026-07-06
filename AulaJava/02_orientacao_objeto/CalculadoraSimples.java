ecd1dc4c8d824de241443333872cb0fc323bc256
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-07-06 08:46:08 -0300

    Add files via upload

---
+++ AulaJava/02_orientacao_objeto/CalculadoraSimples.java
@@ -0,0 +1,8 @@
+public class CalculadoraSimples {
+    public static final String SomarDoisNumeros = null;
+    
+    void SomarDoisNumeros(int numA, int numB){
+        int resultado = numA + numB;
+        System.out.println("Resultado da soma: " + resultado);
+    }     
+}
