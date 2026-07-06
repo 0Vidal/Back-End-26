519c3acd3f51c11927e72693134a42d867180616
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-07-06 08:52:56 -0300

    Add files via upload

---
+++ AulaJava/02_orientacao_objeto/Aula12_Construtores.java
@@ -0,0 +1,6 @@
+public class Aula12_Construtores {
+    public static void main(String[] args) {
+        VideoGame console = new VideoGame("PlayStation 5", 4000.0);
+        System.out.println("Console cadastrado: " + console.nome + " preço: " + console.preco);
+    }
+}
---
+++ AulaJava/02_orientacao_objeto/VideoGame.java
@@ -0,0 +1,11 @@
+public class VideoGame {
+    String nome;
+    double preco;
+
+
+    VideoGame(String nomeInformado, double precoInformado){
+        nome = nomeInformado;
+        preco = precoInformado;
+    }
+    
+}
